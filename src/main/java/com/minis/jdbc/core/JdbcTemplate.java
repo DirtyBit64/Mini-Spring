2bf39fc0ec54be4c6332cd4434f5b3493eb6c065
DirtyBit <ljx@nuist.edu.cn>
2024-06-09 21:00:26 +0800

    增强JdbcTemplate--->增加对返回结果集合的处理接口

--- src/main/java/com/minis/jdbc/core/JdbcTemplate.java
+++ src/main/java/com/minis/jdbc/core/JdbcTemplate.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.List;
 
 @NoArgsConstructor
 public class JdbcTemplate {
@@ -62,6 +63,40 @@ public class JdbcTemplate {
         }
 
         return null;
+    }
 
+    // 提供集合查询
+    public <T> List<T> query(String sql, Object[] args, RowMapper<T> rowMapper) {
+        RowMapperResultSetExtractor<T> resultExtractor = new RowMapperResultSetExtractor<>(rowMapper);
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs;
+
+        try {
+            //建立数据库连接
+            con = dataSource.getConnection();
+
+            //准备SQL命令语句
+            pstmt = con.prepareStatement(sql);
+            //设置参数
+            ArgumentPreparedStatementSetter argumentSetter = new ArgumentPreparedStatementSetter(args);
+            argumentSetter.setValues(pstmt);
+            //执行语句
+            rs = pstmt.executeQuery();
+
+            //数据库结果集映射为对象列表，返回
+            return resultExtractor.extractData(rs);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                pstmt.close();
+                con.close();
+            } catch (Exception e) {
+            }
+        }
+        return null;
     }
 }
---
+++ src/main/java/com/minis/jdbc/core/ResultSetExtractor.java
@@ -0,0 +1,9 @@
+package com.minis.jdbc.core;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+// 把JDBC返回的ResultSet数据集映射为一个集合对象
+public interface ResultSetExtractor<T> {
+    T extractData(ResultSet rs) throws SQLException;
+}
